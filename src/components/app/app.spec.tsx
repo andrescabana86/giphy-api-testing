c2c05c1e664aecde58679ebfb971847b42630f60
andrescabana86 <andrescabana86@gmail.com>
2020-11-20 20:07:07 -0300

    test: add <App /> test
    Resolve: #3

---
+++ src/components/app/app.spec.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import { App } from '@components/app/app'
+
+describe('App', () => {
+  afterEach(cleanup)
+
+  it('should render App component', () => {
+    const { asFragment } = render(<App />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('should show title "Welcome to my boilerplate"', function () {
+    const { getByTestId  } = render(<App />)
+    const title = getByTestId('title')
+    expect(title).toHaveTextContent('Welcome to my boilerplate')
+    expect(title).toHaveClass('title')
+    expect(title).toHaveAttribute('data-testid', 'title')
+  })
+})
