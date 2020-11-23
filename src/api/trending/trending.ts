3a10237aa9a496ea4ef029019205fb217c5bb9b8
andrescabana86 <andrescabana86@gmail.com>
2020-11-23 15:20:55 -0300

    feat: getTrending() endpoint
    create fn to fetch trending results from Giphy API
    serialize the results
    
    Resolve: #10

---
+++ src/api/trending/trending.ts
@@ -0,0 +1,7 @@
+import { TResponse } from '@api/types/giphy.type'
+import { trendingSerializer } from './trending.serializer'
+
+export const getTrending = () =>
+  fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=10`)
+    .then((response) => response.json())
+    .then((response: TResponse) => trendingSerializer(response))
