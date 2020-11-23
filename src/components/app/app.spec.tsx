1239e58cec8b71950263cbd2b0b503b79a70fa2a
andrescabana86 <andrescabana86@gmail.com>
2020-11-23 19:02:41 -0300

    test: update <App /> tests
    Resolve: #23

--- src/components/app/app.spec.tsx
+++ src/components/app/app.spec.tsx
@@ -6,15 +6,11 @@ describe('App', () => {
   afterEach(cleanup)
 
   it('should render App component', () => {
+    // Arrange
+    // Act
     const { asFragment } = render(<App />)
-    expect(asFragment()).toMatchSnapshot()
-  })
 
-  it('should show title "Welcome to my boilerplate"', () => {
-    const { getByTestId } = render(<App />)
-    const title = getByTestId('title')
-    expect(title).toHaveTextContent('Welcome to my boilerplate')
-    expect(title).toHaveClass('title')
-    expect(title).toHaveAttribute('data-testid', 'title')
+    // Assert
+    expect(asFragment()).toMatchSnapshot()
   })
 })
