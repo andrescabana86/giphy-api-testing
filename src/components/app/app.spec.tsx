209ae1d7102e56dc0545708c9bd0718c74672432
andrescabana86 <andrescabana86@gmail.com>
2020-11-24 00:55:22 -0300

    feat: integrate the list with all components
    integrade GifList, GifPreview with GifListContainer and FavoritesContext
    
    Resolve: #32

--- src/components/app/app.spec.tsx
+++ src/components/app/app.spec.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import { App } from '@components/app/app'
+import { disableFetchMocks, enableFetchMocks } from 'jest-fetch-mock'
+
+beforeAll(enableFetchMocks)
+afterAll(disableFetchMocks)
 
 describe('App', () => {
   afterEach(cleanup)
 
   it('should render App component', () => {
     // Arrange
+    fetchMock.mockResponseOnce(JSON.stringify({ data: [] }))
+
     // Act
     const { asFragment } = render(<App />)
 
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { GifListContainer } from '@containers/gif-list/gif-list'
+import { GifList } from '@components/gif-list/gif-list'
+import { SearchInput } from '@components/search-input/search-input'
 import './app.sass'
 
 export class App extends React.PureComponent {
   render() {
     return (
-      <h1 data-testid="title" className="title">Welcome to my boilerplate</h1>
+      <main className="app">
+        <GifListContainer>
+          {({ state, ...container }) => (
+            <>
+              {!!state.favorites.length && (
+                <>
+                  <h1 className="app__favorites">Favorites</h1>
+                  <GifList list={state.favorites} loading={state.loading} />
+                </>
+              )}
+
+              {/* general list */}
+              <h1 className="app__gif-list">Giphy Loader</h1>
+
+              {/* search input */}
+              <section className="app__search">
+                <span className="app__search-caption">Search for a Gif:</span>
+                <SearchInput onSearch={container.onSearch} onClear={container.onClear} />
+              </section>
+
+              <GifList list={state.list} loading={state.loading} />
+            </>
+          )}
+        </GifListContainer>
+      </main>
     )
   }
 }
